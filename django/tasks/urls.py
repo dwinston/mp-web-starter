d103e79555cf95dd14723b7d9ea37f485ac6eacd
Donny Winston <dwinston@alum.mit.edu>
2015-06-25 09:59:08 -0700

    Add commentary for urls.py

--- django/tasks/urls.py
+++ django/tasks/urls.py
@@ -1,3 +1,12 @@
+# Maps URL patterns to functions in views.py
+#
+# For example, visiting https://materialsproject.org/tasks/mp-24850 will
+# trigger a call to views.tasks with keyword arg task_id="mp-24850".
+#
+# This file assumes a urls.py file at the project level with a pattern like
+# (r'^tasks/', include('tasks.urls')) so that this file is for the "tasks/" URL
+# namespace.
+
 from django.conf.urls import *
 
 urlpatterns = patterns('tasks.views',
